04582e60a0871a8d592115da3623ff827a74582c
alexbasista <abasista@hashicorp.com>
2022-08-02 10:12:14 -0400

    add outputs ds

--- dev/app-web/main.tf
+++ dev/app-web/main.tf
@@ -11,6 +11,11 @@ provider "random" {
   # Configuration options
 }
 
+data "tfe_outputs" "foo" {
+  organization = var.tfe_organization
+  workspace    = var.tfe_outputs_workspace
+}
+
 resource "random_pet" "app_web" {
     length    = 2
     prefix    = "web"
---
+++ dev/app-web/variables.tf
@@ -0,0 +1,9 @@
+variable "tfe_organization" {
+    type        = string
+    description = "Name of TFC/TFE Organization."
+}
+
+variable "tfe_outputs_workspace" {
+    type        = string
+    description = "Name of TFC/TFE Workspace to retrieve output values from."
+}
